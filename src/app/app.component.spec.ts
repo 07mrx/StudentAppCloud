65795da0385a067f4ed1c48110dcb8ca1cc11991
Marin Roxana <roxana.marin@cegeka.com>
2019-11-24 21:09:31 +0200

    Serverless deploy static website to s3

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'UserApp'`, () => {
+  it(`should have as title 'StudentApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('UserApp');
+    expect(app.title).toEqual('StudentApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('UserApp app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('StudentApp app is running!');
   });
 });
